c4b7f9ce419574b43fa9ad1c0447e7fe00970ff8
nanyan375 <32097688+nanyan375@users.noreply.github.com>
2018-05-21 23:49:50 +0800

    缩进有点问题

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Length, Email, Required
 
 class LoginForm(FlaskForm):
-	email=StringField('email',validators=[Required(),Email()])
-	password=PasswordField('passowrd',validators=[Required(),Length(6,24)])
-	remember_me = BooleanField('remember_me')
+    email=StringField('email',validators=[Required(),Email()])
+    password=PasswordField('passowrd',validators=[Required(),Length(6,24)])
+    remember_me = BooleanField('remember_me')
     submit = SubmitField('submit') 
 
     def validate_email():
